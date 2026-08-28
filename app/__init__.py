6062d8e0737fad1ce9ac9d557786c63198acc1d5
Rsantos081 <novo-ruan.santostech@gmail.com>
2026-08-28 14:37:49 -0300

    Adicionando Estrura e layout ao projeto

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask,jsonify
+from flask import Flask,jsonify, render_template
 from flask_cors import CORS
 from app.extensions import db, login_manager
 
@@ -17,6 +17,10 @@ def create_app():
         supports_credentials= True,
         origins=['http://127.0.0.1:8000', 'http://localhost:8000']   
     )
+    
+    @app.route('/')
+    def index():
+        return render_template('index.html')
     
     db.init_app(app)
     login_manager.init_app(app)
